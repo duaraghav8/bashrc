d7deaafa2e6e5206d67aa59c1a0cc84a455f5416
Raghav Dua <duaraghav8@gmail.com>
2016-06-14 15:05:47 +0530

    Update .bashrc.sh

--- .bashrc.sh
+++ .bashrc.sh
@@ -1,6 +1,3 @@
-#----------------------------------------------------------------------------------------------
-#Starting here, everything's written by Raghav as part of personal configuration applied upon bash startup
-
 #git aliases
 alias gi='git init'
 alias grao='git remote add origin'
@@ -10,14 +7,23 @@ alias gc='git commit'
 alias gs='git status'
 alias gc='git checkout'
 alias gb='git branch'
+alias gfu='git fetch upstream'
+alias gm='git merge'
+alias ga='git add'
 
-echo "Heyyyyyyy sexy!"
-echo "Here, revise these git aliases: "
-echo "gi='git init'
-grao='git remote add origin'
-grau='git remote add upstream'
-gpo='git push origin'
-gc='git commit'
-gs='git status'
-gc='git checkout'
-gb='git branch'"
+function aliases () {
+        echo "Heyyyyyyy sexy!"
+        echo "Here, revise these git aliases: "
+        echo "
+        gi='git init'
+        grao='git remote add origin'
+        grau='git remote add upstream'
+        gpo='git push origin'
+        gc='git commit'
+        gs='git status'
+        gc='git checkout'
+        gb='git branch'
+        gfu='git fetch upstream'
+        gm='git merge'
+        ga='git add'"
+}
