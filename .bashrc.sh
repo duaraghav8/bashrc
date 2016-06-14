ee36c9ff07eea44d59c2fdc9ba98e991c4bd0f0d
Raghav Dua <duaraghav8@gmail.com>
2016-06-14 11:40:46 +0530

    initial commit - add git aliases

---
+++ .bashrc.sh
@@ -0,0 +1,23 @@
+#----------------------------------------------------------------------------------------------
+#Starting here, everything's written by Raghav as part of personal configuration applied upon bash startup
+
+#git aliases
+alias gi='git init'
+alias grao='git remote add origin'
+alias grau='git remote add upstream'
+alias gpo='git push origin'
+alias gc='git commit'
+alias gs='git status'
+alias gc='git checkout'
+alias gb='git branch'
+
+echo "Heyyyyyyy sexy!"
+echo "Here, revise these git aliases: "
+echo "gi='git init'
+grao='git remote add origin'
+grau='git remote add upstream'
+gpo='git push origin'
+gc='git commit'
+gs='git status'
+gc='git checkout'
+gb='git branch'"
